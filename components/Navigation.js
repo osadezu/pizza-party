f48bd01f458a99677efe886892eabf3ad62c9960
osadezu <osadezu@gmail.com>
2022-03-03 16:42:54 -0500

    Animate hiding menu

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import useUser from '../lib/useUser';
 
+import SvgPizzaIcon from '../assets/pizza-icon.svg';
+import SvgMenuIcon from '../assets/menu-icon.svg';
+
 export default function Navigation() {
+  const [show, setShow] = useState(false);
   const router = useRouter();
   const { user, mutateUser } = useUser();
 
   // console.log('Nav logging pathame:', router.pathname);
 
   if (!user?.isLoggedIn) {
-    return <div id='navigation'></div>;
+    return null;
   }
 
   return (
-    <div id='navigation'>
+    <div id='navigation' className={show ? '' : 'hidden'}>
+      <div className='nav-buttons'>
+        <button
+          type='button'
+          className='menu-toggle'
+          onClick={() => setShow(!show)}>
+          <h3>Menu</h3>
+        </button>
+        <SvgPizzaIcon onClick={() => setShow(!show)} />
+      </div>
       <ul>
         <li>
           <h3>Team Time</h3>
         </li>
         <li className='link'>
           <Link href='/team'>
-            <a className={router.pathname == '/team' ? 'active' : null}>
+            <a
+              className={router.pathname == '/team' ? 'active' : null}
+              onClick={() => setShow(!show)}>
               Rumpus
             </a>
           </Link>
         </li>
         <li className='link'>
-          <p>🚧 Map</p>
+          <p>Map 🚧</p>
           <span>coming soon!</span>
         </li>
         {user?.isAdmin && (
           <li className='link'>
             <Link href='/team/edit'>
-              <a className={router.pathname == '/team/edit' ? 'active' : ''}>
+              <a
+                className={router.pathname == '/team/edit' ? 'active' : ''}
+                onClick={() => setShow(!show)}>
                 Edit Team
               </a>
             </Link>
@@ -52,7 +69,9 @@ export default function Navigation() {
         </li>
         <li className='link'>
           <Link href='/member/edit'>
-            <a className={router.pathname == '/member/edit' ? 'active' : ''}>
+            <a
+              className={router.pathname == '/member/edit' ? 'active' : ''}
+              onClick={() => setShow(!show)}>
               Profile
             </a>
           </Link>
